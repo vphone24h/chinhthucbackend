280ff235ef8146f9c3466ef8bce73d7c3cb010c5
Lê Vũ <vphone24h@192.168.1.230.non-exists.ptr.local>
2025-05-31 19:32:38 +0700

    ♻️ Đổi tên NhapHang.jsx để ép Vercel rebuild

--- server.js
+++ server.js
@@ -18,6 +18,7 @@ const app = express();
 const allowedOrigins = [
   'http://localhost:5174',
   'https://chinhthuc-jade.vercel.app',
+  'https://chinhthuc-git-main-vphone24hs-projects.vercel.app/',
 ];
 
 app.use(cors({
